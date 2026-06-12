50bd190c373d2b328b104a06777398dfea1260ad
bulletbornil-tech <bulletbornil@gmail.com>
2026-06-13 00:50:58 +0600

    Initialize Express server with M3U playlist endpoint
    Set up an Express server to serve M3U playlist.

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const fetch = require("node-fetch");
+
+const app = express();
+app.use(cors());
+
+const M3U_URL = "https://go.skym3u.top/k98v.m3u";
+
+app.get("/playlist", async (req, res) => {
+    try {
+        const response = await fetch(M3U_URL);
+        const data = await response.text();
+        res.send(data);
+    } catch (err) {
+        res.status(500).send("Error");
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server running");
+});
